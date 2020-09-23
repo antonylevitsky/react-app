eeb31c7367b3fd7f46a1ee05b7ad6642636ebfec
alevitskiy <alevitskiy@xbsoftware.by>
2020-09-23 12:36:48 +0300

    first-commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Header = () => {
+    return (
+        <div className="app__header">
+            Header
+        </div>
+    )
+}
+
+export default Header;
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+
+const Navbar = () => {
+    return (
+        <div className="app__nav nav">
+            <div className="nav__item">
+                Main
+            </div>
+
+            <div className="nav__item">
+                Product
+            </div>
+
+            <div className="nav__item">
+                About
+            </div>
+
+            <div className="nav__item">
+                Contact
+            </div>
+        </div>
+    )
+}
+
+export default Navbar;
